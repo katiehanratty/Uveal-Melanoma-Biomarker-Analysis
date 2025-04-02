20d9181052c2c157961eed12f5c3a0a79907b753
katiehanratty <katie.hanratty6@mail.dcu.ie>
2025-04-02 20:38:49 +0100

    Update FastQC.sh

--- FastQC.sh
+++ FastQC.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+#RUNNING FASTQC
+
 # Make a new directory for the raw fastq files  
 # Within this directory, user should create another directory for fastqc outputs
 mkdir -p ~/raw_fastq/FASTQC
 # Import raw paired fastq files manually
-
-# Running FastQC 
 
 # Run fastqc, and direct the outputs of the command to user's FASTQC subdirectory
 fastqc ~/raw_fastq/*fastq.gz --outdir=~/raw_fastq/FASTQC
