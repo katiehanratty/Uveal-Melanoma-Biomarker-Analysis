1377bcb34ff0b56110463c18769e8539d9b2aa48
katiehanratty <katie.hanratty6@mail.dcu.ie>
2025-06-27 11:44:44 +0100

    Update VEP.sh

--- VEP.sh
+++ VEP.sh
@@ -2,11 +2,15 @@
 
 #RUNNING VARIANT EFFECT PREDICTOR
 
-#set working directory to users VCF directory
+#MANUALLY SET WORKING DIRECTORY TO USERS VCF DIRECTORY 
+
 #change file locations as required
 for i in *.vcf; do 
 vep --cache -i $i -fa ~/alignment/reference/hg38.fa \
 --verbose --dir_cache ~/cache \
 -o ~/mutect2/VEP/${i%.vcf}.vep \
---everything --fork 4 
+--everything --fork 4 --vcf
 done
+
+#updated on 27/06 to chang eoutput to VCF (--vcf) . better as you only need to handle one file
+#to see the filtering on the vcf vs the vep. 
